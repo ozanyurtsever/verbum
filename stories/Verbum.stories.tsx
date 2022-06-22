c283d8f079f86d2548dc48c99f51c6c0966e7dd7
Omkar Shelar <omkarshelar24@gmail.com>
2022-06-22 21:13:09 +0530

    Fix storybook build error
    Currently we can't run `npm run storybook` coz the build fails.

--- stories/Verbum.stories.tsx
+++ stories/Verbum.stories.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Meta } from '@storybook/react';
 import { EditorComposer, Editor } from '../src';
-import PlaygroundApp from '../src/App';
 import ToolbarPlugin from '../src/plugins/ToolbarPlugin/ToolbarPlugin';
 import AlignDropdown from '../src/plugins/ToolbarPlugin/components/AlignDropdown';
-import { LexicalEditor } from 'lexical';
 import InsertDropdown from '../src/plugins/ToolbarPlugin/components/InsertDropdown';
 
-const meta: Meta = {
-  title: 'Welcome',
-  component: PlaygroundApp,
+export default {
+  title: 'Verbum',
 };
-
-export default meta;
 
 export const FullEditor = () => (
   <EditorComposer>
