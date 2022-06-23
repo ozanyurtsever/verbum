010e87c5ac8f6de75f4b6843b700ff343197d9e8
Ozan Yurtsever <ozanyurtsever95@gmail.com>
2022-06-23 11:35:45 +0300

    prettier formating applied to avoid CI errors

--- src/libdefs/globals.d.ts
+++ src/libdefs/globals.d.ts
@@ -11,4 +11,4 @@ declare module '*.jpg' {
   export default content;
 }
 
-export type Spread<T1, T2> = {[K in Exclude<keyof T1, keyof T2>]: T1[K]} & T2;
+export type Spread<T1, T2> = { [K in Exclude<keyof T1, keyof T2>]: T1[K] } & T2;
