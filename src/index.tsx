0aa4ff082e52a3b1cb9c138e406249ef707de513
Ozan Yurtsever <ozanyurtsever95@gmail.com>
2022-06-24 13:49:27 +0300

    InsertDropdown and AlignDropdown components exported as well

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import EditorComposer from './EditorComposer';
 import Editor from './Editor';
 import ToolbarPlugin from './plugins/ToolbarPlugin/ToolbarPlugin';
+import InsertDropdown from './plugins/ToolbarPlugin/components/InsertDropdown';
+import AlignDropdown from './plugins/ToolbarPlugin/components/AlignDropdown';
+import * as ToolbarTypes from './types';
 
-export { EditorComposer, Editor, ToolbarPlugin };
+export {
+  EditorComposer,
+  Editor,
+  ToolbarPlugin,
+  InsertDropdown,
+  AlignDropdown,
+  ToolbarTypes,
+};
