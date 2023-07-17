763ecec16c1ae0a4a69f4a0958e3252eded473a9
halykon <37235804+halykon@users.noreply.github.com>
2023-07-17 19:24:43 +0000

    feat(locale): add german translations

--- src/locale/index.tsx
+++ src/locale/index.tsx
@@ -3,10 +3,11 @@ import EN from './languages/en.json';
 import FR from './languages/fr.json';
 import PTBR from './languages/ptBr.json';
 import RU from './languages/ru.json';
+import DE from './languages/de.json';
 
 export const resources = {
   ar: {},
-  de: {},
+  de: DE,
   en: EN,
   es: {},
   fr: FR,
